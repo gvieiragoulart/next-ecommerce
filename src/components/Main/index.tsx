5f2b5f83e3e2689aa6c2053a937fe901c1190067
gabriel vieira <vieirabiel2009@hotmail.com>
2024-12-19 00:04:38 -0300

    teste

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 const Main = () => (
-    <main>
-        <h1>React Avançado</h1>
-    </main>
+  <main>
+    <h1>React Avançado</h1>
+  </main>
 )
 
 export default Main
